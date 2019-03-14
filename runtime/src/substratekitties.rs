79f448958ab48cd3ca0e9b0ab4b455bd4db6783f
Ryo Komiyama <komikomi.mikomiko@gmail.com>
2019-03-14 22:15:18 +0900

    Update: #1 Basics, Storage Mapping

--- runtime/src/substratekitties.rs
+++ runtime/src/substratekitties.rs
@@ -1,4 +1,4 @@
-use support::{decl_storage, decl_module, StorageValue, dispatch::Result};
+use support::{decl_storage, decl_module, StorageMap, dispatch::Result};
 use system::ensure_signed;
 
 pub trait Trait: system::Trait {}
@@ -6,7 +6,7 @@ pub trait Trait: system::Trait {}
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
         // Declare storage and getter functions here
-        Value: u64;
+        Value: map T::AccountId => u64;
     }
 }
 
@@ -14,9 +14,9 @@ decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         // Declare public functions here
         pub fn set_value(origin, input_value: u64) -> Result {
-            let _sender = ensure_signed(origin)?;
+            let sender = ensure_signed(origin)?;
 
-            <Value<T>>::put(input_value);
+            <Value<T>>::insert(sender, input_value);
 
             Ok(())
         }

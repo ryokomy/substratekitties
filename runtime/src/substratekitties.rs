61f747ec8c458144c1d52a1167390a8cee5e598d
Ryo Komiyama <komikomi.mikomiko@gmail.com>
2019-03-14 23:54:43 +0900

    Update: #1 Basics, Stroing a Structure

--- runtime/src/substratekitties.rs
+++ runtime/src/substratekitties.rs
@@ -1,23 +1,37 @@
 use support::{decl_storage, decl_module, StorageMap, dispatch::Result};
 use system::ensure_signed;
+use runtime_primitives::traits::{As, Hash};
+use parity_codec::{Encode, Decode};
 
-pub trait Trait: system::Trait {}
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+pub struct Kitty<Hash, Balance> {
+    id: Hash,
+    dna: Hash,
+    price: Balance,
+    gen: u64,
+}
+
+pub trait Trait: balances::Trait {}
 
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
-        // Declare storage and getter functions here
-        Value: map T::AccountId => u64;
+        MyKitty: map T::AccountId => Kitty<T::Hash, T::Balance>;
     }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        // Declare public functions here
-        pub fn set_value(origin, input_value: u64) -> Result {
+        fn create_kitty(origin) -> Result {
             let sender = ensure_signed(origin)?;
 
-            <Value<T>>::insert(sender, input_value);
+            let new_kitty = Kitty {
+                id: <T as system::Trait>::Hashing::hash_of(&0),
+                dna: <T as system::Trait>::Hashing::hash_of(&0),
+                price: <T::Balance as As<u64>>::sa(0),
+                gen: 0,
+            };
 
+            <MyKitty<T>>::insert(sender, new_kitty);
             Ok(())
         }
     }

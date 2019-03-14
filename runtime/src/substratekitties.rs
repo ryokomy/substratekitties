e5355ae74b703346744f68b9478eea4fff6f000a
Ryo Komiyama <komikomi.mikomiko@gmail.com>
2019-03-14 22:01:36 +0900

    Update: #1 Basics, Storing a Value

--- runtime/src/substratekitties.rs
+++ runtime/src/substratekitties.rs
@@ -1,4 +1,5 @@
-use support::{decl_storage, decl_module};
+use support::{decl_storage, decl_module, StorageValue, dispatch::Result};
+use system::ensure_signed;
 
 pub trait Trait: system::Trait {}
 
@@ -12,5 +13,12 @@ decl_storage! {
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         // Declare public functions here
+        pub fn set_value(origin, input_value: u64) -> Result {
+            let _sender = ensure_signed(origin)?;
+
+            <Value<T>>::put(input_value);
+
+            Ok(())
+        }
     }
 }
